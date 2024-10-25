125b18476863a15f6da3c910a075c56e2a1d4ed3
joseph daniel <josephdaniel@gmail.com>
2024-10-25 12:46:20 -0300

    feature get na trava

--- routes/trava_route.js
+++ routes/trava_route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const Trava = require("../models/trava_model");
-const TravaRouter = express.Router(); 
+const TravaRouter = express.Router();
 
+// Endpoint para criar uma nova trava
 TravaRouter.post("/api/trava", async (req, res) => {
     const { trava } = req.body;
 
@@ -12,11 +13,11 @@ TravaRouter.post("/api/trava", async (req, res) => {
 
     try {
         const horaAtual = new Date();
-        horaAtual.setHours(horaAtual.getHours() - 3);
+        horaAtual.setHours(horaAtual.getHours() - 3); // Ajuste para GMT-3 se necessário
+
         const novaTrava = new Trava({
             trava,
             hora: horaAtual,
-
         });
 
         await novaTrava.save();
@@ -27,4 +28,16 @@ TravaRouter.post("/api/trava", async (req, res) => {
     }
 });
 
+// Endpoint para obter todas as travas
+TravaRouter.get("/api/trava", async (req, res) => {
+    try {
+        const travas = await Trava.find(); // Busca todas as travas no banco de dados
+        res.status(200).json(travas); // Retorna as travas como JSON
+    } catch (error) {
+        console.error(error); // Log do erro para depuração
+        res.status(500).json({ message: 'Erro ao buscar travas', error: error.message });
+    }
+});
+
 module.exports = TravaRouter;
+
